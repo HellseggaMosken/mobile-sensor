3781aa79b69cdd29ff390f8bad47935c35478a09
HellseggaMosken <thetemp@qq.com>
2022-01-09 20:32:59 +0800

    modify gnss stream

--- lib/sensor/gnss.dart
+++ lib/sensor/gnss.dart
@@ -1,27 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:location/location.dart';
 
 class GNSS {
   static final Location _location = Location();
   static final GNSS _instance = GNSS._();
+
+  final _gnssStreamCtl = StreamController<LocationData>.broadcast();
+
   GNSS._() {
     _location.changeSettings(
       accuracy: LocationAccuracy.high,
       interval: 100, // 10Hz
     );
+
+    // _location.onLocationChanged only change when location changed, the _timer
+    // and _gnssStreamCtl can make a stream of constant frequency
+    Timer.periodic(const Duration(milliseconds: 100), (timer) async {
+      var _lc = await _location.getLocation();
+      _gnssStreamCtl.sink.add(_lc);
+    });
   }
   factory GNSS() => _instance;
 
-  Stream<double> latitude() => _location.onLocationChanged.map((e) {
-        print(e);
-        return e.latitude!;
-      });
+  Stream<double> latitude() => _gnssStreamCtl.stream.map((e) => e.latitude!);
 
-  Stream<double> longitude() =>
-      _location.onLocationChanged.map((e) => e.longitude!);
+  Stream<double> longitude() => _gnssStreamCtl.stream.map((e) => e.longitude!);
 
-  Stream<double> altitude() =>
-      _location.onLocationChanged.map((e) => e.altitude!);
+  Stream<double> altitude() => _gnssStreamCtl.stream.map((e) => e.altitude!);
 
   static Future<bool> initGnssSensor() async {
     bool _serviceEnabled;
